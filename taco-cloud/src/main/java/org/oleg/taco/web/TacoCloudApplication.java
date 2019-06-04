e3380037652e6d21022bc00596ff896c656fb1cd
Unit <Unit@Legion>
2019-06-04 09:35:04 +0300

    Changed position of controllers

---
+++ taco-cloud/src/main/java/org/oleg/taco/web/TacoCloudApplication.java
@@ -0,0 +1,15 @@
+package org.oleg.taco.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication.class, args);
+
+	}
+
+}
+
